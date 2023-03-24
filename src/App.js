9a4d84a0db2688eaafbed4cb18e28fea62a84e53
nomayen <nomayen.ohin@gmail.com>
2023-03-24 12:57:07 +0600

    error page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
 import React from 'react'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Header from './components/Header'
+import Home from './components/Home'
+import Books from './components/Books'
+import ErrorPage from './components/ErrorPage'
+
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element:<Home></Home>,
+    errorElement:<ErrorPage></ErrorPage>
+    
+  },
+  {
+    path:'/books',
+    element:<Books></Books>
+  },
+
+])
 
 function App() {
-  return <h1> Welcome to proReader</h1>
+  return (
+    <div>
+      <Header></Header>
+      <RouterProvider router={router}></RouterProvider>
+    </div>
+  )
+
+
+
 }
 
 export default App
